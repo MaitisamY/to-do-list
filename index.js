5eb8925cc0044e337355f9a5859c3cf792a19680
M aitisam Y <m.aitisamyaseen@gmail.com>
2023-10-17 18:03:38 +0500

    1- Added some functionality to get and post methods. 2- Made the web application responsive. 3- Completed final modifications.

--- index.js
+++ index.js
@@ -5,33 +5,38 @@ const app = express();
 const port = 3000;
 var tasks = [];
 var workTasks = [];
+const date = new Date();
+const toDate = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format(date);
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-});
-
-app.get("/work", (req, res) => {
-    res.render("work.ejs");
-});
-
-app.post("/", (req, res) => {
-    let newTasks = [].concat(tasks, req.body["task"]);
-    tasks = newTasks;
-        res.render("index.ejs", {
-            task: tasks
+function doTheStuff(req, res, next) {
+    const dateOne = new Date();
+    const toDateOne = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format(date);
+    const monthName = months[date.getMonth()];
+    if(toDate == toDateOne) {
+        app.get("/", (req, res) => {
+            res.render("index.ejs", {dateToday : toDateOne, taskList : tasks});
         });
-});
-
-app.post("/work", (req, res) => {
-    let newTask = [].concat(workTasks, req.body["workTask"]);
-    workTasks = newTask;
-        res.render("work.ejs", {
-            task: workTasks
+        app.get("/work", (req, res) => {
+            res.render("work.ejs", { thisMonth : monthName, taskList : workTasks});
         });
-});
+        app.post("/", (req, res) => {
+            var newTasks = [].concat(tasks, req.body["task"]);
+            tasks = newTasks;
+                res.redirect("/");
+        });
+        app.post("/work", (req, res) => {
+            let newTask = [].concat(workTasks, req.body["workTask"]);
+            workTasks = newTask;
+                res.redirect("/work");
+        });
+    }
+    next();
+}
+app.use(doTheStuff);
 
 app.listen(port, () => {
     console.log(`Listening at: ${port}`);
